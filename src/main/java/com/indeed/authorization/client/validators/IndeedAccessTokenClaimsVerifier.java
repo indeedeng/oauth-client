ee27448f798b37a5289e7b2fddb172cbd5930609
Alan <ahuang@indeed.com>
2022-06-10 16:08:42 -0600

    modified IndeedAccessTokenClaimsVerifier to be more readable

--- src/main/java/com/indeed/authorization/client/validators/IndeedAccessTokenClaimsVerifier.java
+++ src/main/java/com/indeed/authorization/client/validators/IndeedAccessTokenClaimsVerifier.java
@@ -10,9 +10,8 @@ import com.nimbusds.oauth2.sdk.ParseException;
 import com.nimbusds.oauth2.sdk.id.ClientID;
 import com.nimbusds.oauth2.sdk.id.Issuer;
 
-import java.time.Instant;
 import java.util.Arrays;
-import java.util.Date;
+import java.util.HashSet;
 
 import static com.indeed.authorization.client.claims.IndeedAccessTokenClaimSet.AZP_CLAIM_NAME;
 import static com.indeed.authorization.client.claims.IndeedAccessTokenClaimSet.EXP_CLAIM_NAME;
@@ -89,26 +88,52 @@ public class IndeedAccessTokenClaimsVerifier
      * @throws BadJWTException If the JWT claims set is rejected.
      */
     private void verifyInternal(final IndeedAccessTokenClaimSet claimsSet) throws BadJWTException {
-        final String actualIssuer = claimsSet.getIssuer().getValue();
-        if (!this.expectedIssuer.getValue().equals(actualIssuer)) {
-            throw new BadJWTException("Unexpected issuer claim: " + claimsSet.getIssuer());
+        final String iss = claimsSet.getIssuer().getValue();
+        if (!this.isValidIssuer(iss)) {
+            this.throwBadJWTException("Unexpected issuer claim", this.expectedIssuer, iss);
         }
 
-        final String actualAuthorizedParty = (String) claimsSet.getClaim(AZP_CLAIM_NAME);
-        if (!this.expectedClientID.getValue().equals(actualAuthorizedParty)) {
-            throw new BadJWTException(
-                    "Unexpected authorized party claim: " + actualAuthorizedParty);
+        final String azp = (String) claimsSet.getClaim(AZP_CLAIM_NAME);
+        if (!isValidAuthorizedParty(azp)) {
+            this.throwBadJWTException(
+                    "Unexpected authorized party claim", this.expectedClientID, azp);
         }
 
-        final String[] actualScopes =
-                ((String) claimsSet.getClaim(SCOPE_CLAIM_NAME)).split(SCOPE_CLAIM_DELIMITER);
-        if (!Arrays.deepEquals(actualScopes, this.expectedScopes)) {
-            throw new BadJWTException("Unexpected scope claim: " + actualAuthorizedParty);
+        final String scope = (String) claimsSet.getClaim(SCOPE_CLAIM_NAME);
+        if (!this.areScopesGranted(scope)) {
+            this.throwBadJWTException(
+                    "Requested scope were not all granted",
+                    Arrays.toString(this.expectedScopes),
+                    scope);
         }
 
-        final Date expirationTime = new Date((long) claimsSet.getClaim(EXP_CLAIM_NAME));
-        if (Date.from(Instant.now()).after(expirationTime)) {
-            throw new BadJWTException("Token has expired at: " + expirationTime);
+        final long exp = (long) claimsSet.getClaim(EXP_CLAIM_NAME);
+        if (this.isExpired(exp)) {
+            this.throwBadJWTException("Token is expired", System.currentTimeMillis(), exp);
         }
+    }
+
+    private boolean isValidIssuer(final String iss) {
+        return this.expectedIssuer.getValue().equals(iss);
+    }
+
+    private boolean isValidAuthorizedParty(final String azp) {
+        return this.expectedClientID.getValue().equals(azp);
+    }
+
+    private boolean areScopesGranted(final String scope) {
+        return new HashSet<>(Arrays.asList(scope.split(SCOPE_CLAIM_DELIMITER)))
+                .containsAll(Arrays.asList(this.expectedScopes));
+    }
+
+    private boolean isExpired(final long exp) {
+        return System.currentTimeMillis() > exp;
+    }
+
+    private <T> void throwBadJWTException(
+            final String description, final T expected, final T actual) throws BadJWTException {
+        throw new BadJWTException(
+                String.format(
+                        "%s: \n" + "expected[%s]\n" + "actual[%s]", description, expected, actual));
     }
 }
